eb0c2de20539ff738cc64d2684e59efe0f267ed4
Andrexx18 <kevin.romerog@uniagustiniana.edu.co>
2025-05-24 19:06:55 +0000

    face para convertir en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { AppProvider } from './Contexto/contexto';
+import { supabase } from './supabase';
 import './App.css';
 
+import Menu from './Componentes/Menu';
 import Aleatorios from './Componentes/Aleatorios';
 import Capturados from './Componentes/Capturados';
 import Favoritos from './Componentes/Favoritos';
 import Lista from './Componentes/Lista';
 import Pokemon from './Componentes/Pokemon';
 import Usuario from './Componentes/Usuario';
-import Menu from './Componentes/Menu';
 import Login from './Componentes/Login';
 import Registro from './Componentes/Registro';
-import { supabase } from './supabase';
+import Administrador from './Componentes/Administrador';
 
 function App() {
   const [usuario, setUsuario] = useState(null);
@@ -24,6 +25,7 @@ function App() {
       setUsuario(session?.user || null);
       setCargando(false);
     }
+
     verificarSesion();
 
     supabase.auth.onAuthStateChange((_event, session) => {
@@ -37,15 +39,20 @@ function App() {
     <AppProvider>
       <Router>
         {usuario && <Menu />}
+
         <Routes>
+          {}
           <Route path="/" element={usuario ? <Lista /> : <Navigate to="/login" />} />
           <Route path="/usuarios" element={usuario ? <Usuario /> : <Navigate to="/login" />} />
           <Route path="/aleatorios" element={usuario ? <Aleatorios /> : <Navigate to="/login" />} />
           <Route path="/capturados" element={usuario ? <Capturados /> : <Navigate to="/login" />} />
           <Route path="/favoritos" element={usuario ? <Favoritos /> : <Navigate to="/login" />} />
           <Route path="/pokemon/:name" element={usuario ? <Pokemon /> : <Navigate to="/login" />} />
+          <Route path="/administrador" element={usuario ? <Administrador /> : <Navigate to="/login" />} />
+
+          {}
           <Route path="/login" element={<Login />} />
-          <Route path="/Registro" element={<Registro />} />
+          <Route path="/registro" element={<Registro />} />
         </Routes>
       </Router>
     </AppProvider>
